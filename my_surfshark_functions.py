18987e26412906300e3197a66576a1c679cd108d
NETkuz42 <netkuz42@gmail.com>
2022-10-06 09:08:30 +1000

    Откорректировал загрузку ссылок на регионы

--- my_surfshark_functions.py
+++ my_surfshark_functions.py
@@ -11,12 +11,13 @@ from datetime import datetime
 
 class MySurf:
 
-    def __init__(self, ID, country_explorer=False, connect_method='random_all', lose_sleep_time=300, ):
+    def __init__(self, ID, country_explorer=False, connect_method='random_all', lose_sleep_time=300):
         self.control: Chrome = None
         self.tab_surf_id = None
         self.tab_setting_id = None
         self.real_agent = None
         self.tab_pars_id = None
+        self.successful_pages = 0
         self.attempts = 0
         self.country_explorer = country_explorer  # Диспетчер стран распределяющий их по скорости
         self.lose_sleep_time = lose_sleep_time  # Вреамя сна если вылезла ошибка сурфа или 3 неудачных коннекта
@@ -211,18 +212,34 @@ class MySurf:
         return surf_session  # Возрващается список 1-страна, 2-IP адрес.
 
     # Удаляет все следы.
-    def connect_error_detect(self, page, status=None, successful_pages=0, max_page=130):
+    def connect_error_detect(self, page, status=None, max_page=10000):
 
         # Запускает переподключение по новому кругу
-        def new_face(new_status):
+        def full_new_face(new_status):
             reload = self.remove_evidence(status=new_status)  # Удаляет все следы
-            self.log(new_status, successful_pages, f"after_{new_status}", reload[0],
+            self.log(new_status, self.successful_pages, f"after_{new_status}", reload[0],
                      reload[2])  # Записывает данные в лог
             sleep(3)
-            reset_pages = 0  # Обнуляет счётчик успешных страниц
-            self.connect_error_detect(page, status=new_status, successful_pages=reset_pages)  # Идуёт на новый круг
-            return reset_pages
+            self.successful_pages = 0  # Обнуляет счётчик успешных страниц
+            self.connect_error_detect(page, status=new_status,)  # Идёт на новый круг
+            return True
 
+        def reload_face():
+            self.control.browser.switch_to.window(self.tab_pars_id),  # Переключаюсь на окно парсинга
+            sleep(1)
+            self.control.browser.get('about:blank'),  # Открывает пустую страницу
+            sleep(2)
+            self.control.browser.switch_to.window(self.tab_setting_id)  # Переключается на окно с настройками
+            sleep(1)
+            self.control.clear_cache(),  # Удаляет весь кэш и куки
+            sleep(1)
+            self.control.browser.switch_to.window(self.tab_pars_id),  # переключается на окно с парсингом
+            sleep(1)
+            self.control.change_fake_agent(),  # Меняет юзер агента на фиктивного
+            sleep(1)
+            self.successful_pages += 1
+            self.connect_error_detect(page, status=None,)
+            return True
         # Проверка на максимальное количество загруженных страниц
         min_page = max_page - self.ID * 10  # Вводит минимальный лимит страниц
         if min_page < max_page:
@@ -230,35 +247,40 @@ class MySurf:
                                           int(max_page))  # Проверяет чтобы минимальный лимит был больше максимального
         else:
             page_limit = max_page  # Если min>max тогда лимит=max.
-        if successful_pages >= page_limit:
+        if self.successful_pages >= page_limit:
             print("ID ", self.ID, " лимит ", f"{page_limit}")
-            successful_pages = new_face("page_limit")  # Если блокировка есть уходит на новый круг
+            full_new_face("page_limit")  # Если блокировка есть уходит на новый круг
+
+        if self.successful_pages % 30 == 0 and self.successful_pages != 0:
+            print("ID ", self.ID, "обновляю лицо", f"стр:{page_limit}")
+            reload_face()
+
         try:
             self.control.browser.get(page)  # Пробует открыть страницу
         except TimeoutException:
             print("ID ", self.ID, " ошибка загрузки ", "страница за указанное время не загрузилась")
-            successful_pages = new_face("empty_page")  # Коннектится под новым лицом
+            full_new_face("empty_page")  # Коннектится под новым лицом
         except WebDriverException:  # Если ошибка при открытии, уходит в рекурсивную функцию смены VPN серверов.
             print("ID ", self.ID, " ошибка загрузки ", "ошибка драйвера")
-            successful_pages = new_face("empty_page")  # Коннектится под новым лицом
+            successful_pages = full_new_face("empty_page")  # Коннектится под новым лицом
         sleep(1)
         surf_lock_status = self.alert_detect()  # Проверяет наличие блокировки сурфом
         if surf_lock_status:
             print("ID ", self.ID, " блокировка ", "surf_alert")
-            successful_pages = new_face("surf_alert")  # Если блокировка есть уходит на новый круг
+            full_new_face("surf_alert")  # Если блокировка есть уходит на новый круг
         sleep(1)
         website_lock_status = self.website_lock_detect()  # Проверяряет наличие блокировки сайтом
         if website_lock_status != False:
             print("ID ", self.ID, " блокировка ", website_lock_status)
-            successful_pages = new_face(website_lock_status)  # Если блокировка есть уходит на новый круг
+            full_new_face(website_lock_status)  # Если блокировка есть уходит на новый круг
         sleep(1)
         website_confirm_status = self.website_confirm_detect()
         if website_confirm_status == False:
             print("ID ", self.ID, "нет подтерждения успеха(ХЗ почему)")
-            successful_pages = new_face("not_confirm")  # Если нет подтверждения успеха,уходит на новый круг
+            full_new_face("not_confirm")  # Если нет подтверждения успеха,уходит на новый круг
 
-        successful_pages += 1
-        return successful_pages  # Возращает значение "успешности" проверки
+        self.successful_pages += 1
+        return True  # Возращает значение "успешности" проверки
 
     # Функция для проверки успешности при прозвоне стран
     def defines_a_lock(self, page,
