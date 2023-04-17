7b802f5a76e4ba416d7bd1b6f2d124d5a5512280
NETkuz42 <netkuz42@gmail.com>
2023-04-17 18:54:47 +1000

    test

--- test.py
+++ test.py
@@ -1,29 +1,36 @@
 from pywinauto import Desktop, Application
-from pywinauto import findwindows
-# from time import sleep
-# # app = Application(backend="uia").start(r"C:\Program Files (x86)\Surfshark\Surfshark.exe")
-# app = findwindows.find_window(title="Surfshark 4.10.1")
-# print(app)
-# app = Application(backend="uia").connect(handle=app)
-# dlg_spec = app["Surfshark"]
-# print(dlg_spec)
-# dlg_spec.print_control_identifiers()
-#
-# from pywinauto import Desktop, Application
+from pywinauto import findwindows, ElementNotFoundError
+from time import sleep
 
-Application().start('explorer.exe "C:\\Program Files"')
 
-# connect to another process spawned by explorer.exe
-# Note: make sure the script is running as Administrator!
-app = Application(backend="uia").connect(path="explorer.exe", title="Program Files")
+def close_popup_another_vpn(surf_windows):
+    try:
+        close_click = surf_windows.child_window(title="Close pop up", auto_id="popup_close_button", control_type="Button")
+        close_click.click_input()
+        print(close_click)
+    except ElementNotFoundError:
+        return False
+    return True
 
-app.ProgramFiles.set_focus()
-common_files = app.ProgramFiles.ItemsView.get_item('Common Files')
-common_files.right_click_input()
-app.ContextMenu.Properties.invoke()
 
-# this dialog is open in another process (Desktop object doesn't rely on any process id)
-Properties = Desktop(backend='uia').Common_Files_Properties
-Properties.print_control_identifiers()
-Properties.Cancel.click()
-Properties.wait_not('visible') # make sure the dialog is closed
+start = Application(backend="uia").start(r"C:\Program Files (x86)\Surfshark\Surfshark.exe")
+sleep(2)
+app = findwindows.find_window(title="Surfshark 4.10.1")
+app = Application(backend="uia").connect(handle=app)
+surf = app["Surfshark"]
+surf.print_control_identifiers()
+# test = app_window.print_control_identifiers()
+# app_window.print_control_identifiers()
+
+albania = surf.child_window(title="Albania", auto_id="location_albania", control_type="Button").wrapper_object()
+albania.click_input()
+
+test = close_popup_another_vpn(surf)
+print(test)
+
+
+sleep(1)
+close_button = surf.child_window(title="Close application", control_type="Button").wrapper_object()
+close_button.click_input()
+
+
