c969058b50565882d67f7a00359a20d021407ccd
NETkuz42 <netkuz42@gmail.com>
2023-04-18 23:58:48 +1000

    рабочие дополнения

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 from pywinauto import Desktop, Application
 from pywinauto import findwindows, ElementNotFoundError
+from pywinauto.timings import always_wait_until_passes
 from time import sleep
 
 
@@ -13,12 +14,24 @@ def close_popup_another_vpn(surf_windows):
     return True
 
 
+# @always_wait_until_passes(4, 2)
+# def ensure_text_changed(ctrl):
+#     if previous_text == ctrl.window_text():
+#         raise ValueError('The ctrl text remains the same while change is expected')
+
+
 Application(backend="uia").start(r"C:\Program Files (x86)\Surfshark\Surfshark.exe")
 sleep(2)
-app = Application(backend="uia", allow_magic_lookup=False).connect(path="Surfshark.exe")
-surf = app["Surfshark"]
-albania = surf['ListBox3'].child_window(title="United States St. Louis", control_type="ListItem").
-albania.select()
+app = Application(backend="uia").connect(path="Surfshark.exe")
+surf_window = app["Surfshark"].print_control_identifiers()
+country_list = surf_window['ListBox3']
+
+slovenia = country_list["Slovenia"].set_focus()
+sleep(2)
+belize = country_list["Belize"].set_focus()
+# test = slovenia.wrapper_object()
+# print(type(test))
+# albania.select()
 # surf["ListBox"].print_control_identifiers()
 # test = app_window.print_control_identifiers()
 # app_window.print_control_identifiers()
