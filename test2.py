5e06d603156e2513837a89189ef87324a79be44b
NETkuz42 <netkuz42@gmail.com>
2023-04-24 10:52:10 +1000

    test

--- test2.py
+++ test2.py
@@ -1,6 +1,11 @@
 import subprocess
 import sys
 
-result = subprocess.run([sys.executable, "-c", "print('ocean')"])
+path_to_speedtest = r"D:\DISTRIB_LOCAL\PROGRAM\speedtest\speedtest.exe"
+servers = {"mts_mos": "librarian.comstar.ru", "dom_spb": "speedtest.spb.ertelecom.ru", "near": ""}
+
+result = subprocess.run([path_to_speedtest, "-o", servers["dom_spb"]], capture_output=True)
+
+print(result)
 
 
