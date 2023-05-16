eb495bd9b6a2fb6219b280cdefdc624520057b32
NETkuz42 <netkuz42@gmail.com>
2023-05-16 15:28:15 +1000

    test

--- my_browsers.py
+++ my_browsers.py
@@ -94,7 +94,8 @@ class Chrome:
 
         def clear_file():  # Сбрасывает кэш и куки удалением файлов
             full_cash_path = fr"{self.path_to_profile}\Default\Cache\Cache_Data"  # Путь к папке кэша
-            shutil.rmtree(full_cash_path)  # Удаляет кэш
+            if os.path.exists(full_cash_path):
+                shutil.rmtree(full_cash_path)  # Удаляет кэш
             sleep(2)
             self.browser.delete_all_cookies()
             sleep(2)
