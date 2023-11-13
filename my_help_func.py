d9cec7cd18c20d0dc493656a797fec99e4f1f750
NETkuz42 <netkuz42@gmail.com>
2023-11-13 19:15:53 +1000

    test

--- my_help_func.py
+++ my_help_func.py
@@ -4,7 +4,7 @@ import numpy as np
 import pandas as pd
 import os
 import shutil
-import my_browsers as mb
+from my_parsing_common import my_browsers as mb
 from gtts import gTTS
 import vlc
 from time import sleep
